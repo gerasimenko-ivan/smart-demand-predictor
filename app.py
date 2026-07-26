57c8e84059d14e56384265c382ba1578ceab8ef9
Ivan G <ivan.grsko@gmail.com>
2026-07-27 11:32:14 +1200

    weather loading added to app.py

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ controller = ProjectController()
 
 try:
     sales_df = controller.load_sales_data()
+    weather_df = controller.load_weather_data()
 
-    st.success("Sales data loaded successfully!")
+    st.success("Sales, weather data loaded successfully!")
 
+    st.header("Sales")
     st.dataframe(sales_df)
+
+    st.header("Weather")
+    st.dataframe(weather_df)
 
 except Exception as e:
     st.error(str(e))
